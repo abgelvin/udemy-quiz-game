0d148a5316401fb45bb3b436908bb03807e24677
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-06-12 17:02:31 -0600

    functional quiz code

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 
-question_list = [
+question_bank = [
     Question(question['text'], question['answer']) for question in question_data
 ]
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions(quiz.question_list):
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score is {quiz.score}/{quiz.question_number}.")
--- quiz_brain.py
+++ quiz_brain.py
@@ -2,16 +2,28 @@
 class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = question_list
 
-    def next_question():
-        question = self.question_list[0].question
-        return input(question)
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        if user_answer.lower() != 'true' or user_answer.lower() != 'false':
+            user_answer = input(f"Please enter True or False: ")
+        self.check_answer(user_answer, current_question.answer)
 
 
-    def check_answer(self):
-        pass
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print('Correct!')
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}.\n\n")
 
-    
-    def is_end(self):
-        pass
+          
+    def still_has_questions(self, question_list):
+        return self.question_number < len(question_list)
+            
