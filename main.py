08f5e748a15036e3d6f7a91fd21fed26180fa016
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-06-12 18:38:48 -0600

    made Question object code clearer

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from quiz_brain import QuizBrain
 import json
 
 
-question_bank = [
-    Question(question['question'], question['correct_answer']) for question in question_data
-]
+question_bank = []
+for question in question_data:
+    question_text = question['question']
+    question_answer = question['correct_answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+    Question(question['question'], question['correct_answer']) 
+
 
 quiz = QuizBrain(question_bank)
 
